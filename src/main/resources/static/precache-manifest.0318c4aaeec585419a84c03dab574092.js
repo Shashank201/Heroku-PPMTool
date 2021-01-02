ff2daf7d0598c62a055712b8a681222d527fe01f
Shashank201 <svarshney17@gmail.com>
2021-01-02 22:31:01 +0530

    axios interceptor added

--- src/main/resources/static/precache-manifest.0318c4aaeec585419a84c03dab574092.js
+++ src/main/resources/static/precache-manifest.0318c4aaeec585419a84c03dab574092.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d33dd480578fae048e80aa1e38b95dcf",
+    "revision": "62dad0cfa3da977f4b59988159b70809",
     "url": "/index.html"
   },
   {
-    "revision": "3dacc4f8015ab2b60511",
+    "revision": "898a0780042d6299d5a7",
     "url": "/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "5e30a77fab86b405c384",
+    "revision": "1fb2ca3b60ae584151b5",
     "url": "/static/css/main.7556a164.chunk.css"
   },
   {
-    "revision": "3dacc4f8015ab2b60511",
-    "url": "/static/js/2.2e97b987.chunk.js"
+    "revision": "898a0780042d6299d5a7",
+    "url": "/static/js/2.b59d2ef5.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/static/js/2.2e97b987.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.b59d2ef5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5e30a77fab86b405c384",
-    "url": "/static/js/main.83830465.chunk.js"
+    "revision": "1fb2ca3b60ae584151b5",
+    "url": "/static/js/main.685abd65.chunk.js"
   },
   {
     "revision": "f0395242fcfe40790f57",
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a73e1a52c93d75ccb9339c8d940e9376.js"
+  "/precache-manifest.0318c4aaeec585419a84c03dab574092.js"
 );
 
 self.addEventListener('message', (event) => {
