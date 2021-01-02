2e35f15ece79579911b21486699e94b4629aad9a
Shashank201 <svarshney17@gmail.com>
2021-01-02 17:37:22 +0530

    react build added in static

---
+++ src/main/resources/static/precache-manifest.f116dd015cf5f8914d8b1d179cbde195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e837432ce224acb966c08ddcb5788e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bd2b9bd6e55abf5b5b7",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "81715c2720097acc6954",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "3bd2b9bd6e55abf5b5b7",
+    "url": "/static/js/2.9c8f06e8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.9c8f06e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81715c2720097acc6954",
+    "url": "/static/js/main.3106ff63.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
